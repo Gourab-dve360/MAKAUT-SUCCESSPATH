de56775dec935ad51cfdb133560a33e613975460
infogourab <gourab.bhatt13@gmail.com>
2023-12-04 11:53:50 +0530

    Done All

---
+++ Blogs/Bl.js
@@ -0,0 +1,2 @@
+// You can add JavaScript functionality if needed
+// This file is intentionally left empty for simplicity
---
+++ Placement/pl.js
@@ -0,0 +1,2 @@
+// You can add JavaScript functionality here if needed
+// For now, it's empty in this example
